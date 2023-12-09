6c5d335bd87d1fb1aa84e8afd41d3c2949977842
NikKowPHP <nik.kow@outlook.com>
2023-12-09 10:37:04 +0100

    feat(App): Establish basic header section layout
    Implement a foundational header section layout within the App component. This addition sets up the structure and initial design for the header section, laying the groundwork for further development and content incorporation.

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,53 @@ import "./App.css";
 
 const App: React.FC = () => {
   const renderHeader = (): JSX.Element => (
-    <section>
-      <div>
-        <div className="hero-text">
-          <span>
-            <h1 className="hero__header">2023</h1>
-          </span>
+    <>
+      <section>
+        <div>
+          <div className="hero-text">
+            <span>
+              <h1 className="hero__header">2023</h1>
+            </span>
+          </div>
+          <div className="hero-text">
+            <span>
+              <h1 className="hero__header">PORTFOLIO</h1>
+            </span>
+          </div>
+          <div className="hero-text">
+            <span>
+              <h1 className="hero__header">MIKITA KAVALIOU</h1>
+            </span>
+          </div>
         </div>
-        <div className="hero-text">
-          <span>
-            <h1 className="hero__header">PORTFOLIO</h1>
-          </span>
+        <div className="panel__container">
+          <div className="panel">
+            <div className="text__wrap reverse">
+              <h1>JUNIOR PHP DEVELOPER</h1>
+            </div>
+          </div>
+          <div className="panel">
+            <div className="text__wrap">
+              <h1>JUNIOR JAVASCRIPT DEVELOPER</h1>
+            </div>
+          </div>
         </div>
-        <div className="hero-text">
-          <span>
-            <h1 className="hero__header">MIKITA KAVALIOU</h1>
-          </span>
+        <div className="panel">
+          <div className="text__wrap reverse">
+            <h1>REACT LARAVEL</h1>
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="panel">
+          <div className="text__wrap">
+            <h1>MIKITA KAVALIOU</h1>
+          </div>
+        </div>
+      </section>
+      <canvas></canvas>
+    </>
   );
 
   return <>{renderHeader()}</>;
-}
+};
 
 export default App;
