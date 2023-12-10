38075c3cc63657411703c48c6a2cac1741b89d10
NikKowPHP <nik.kow@outlook.com>
2023-12-10 09:15:51 +0100

    feat(CanvasAnimation): Add 'setDimensions' function for canvas resizing and coordinate population
    Implement the 'setDimensions' function within the canvas module. This function dynamically resizes the canvas to match the window's dimensions and generates random coordinates to populate the circleArray, ensuring the canvas adapts to the window size and initializes circle coordinates for rendering.

--- src/components/CanvasAnimation.ts
+++ src/components/CanvasAnimation.ts
@@ -12,6 +12,25 @@ interface MouseCoords {
 function lerp(start: number, end: number, t: number) {
   return start * (1 - t) + end * t;
 }
+function setDimensions(canvas: HTMLCanvasElement) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const circleArray: Coordinates[] = [];
+  for (let i = 0; i < 100; i++) {
+    let x = 0;
+    let y = 0;
+    while (x < canvas.width * 0.2 || x > canvas.width * 0.8) {
+      x = Math.random() * canvas.width;
+    }
+    while (x < canvas.height * 0.2 || y > canvas.height * 0.8) {
+      y = Math.random() * canvas.height;
+    }
+    x = Math.random() * canvas.width;
+    y = Math.random() * canvas.height;
+    circleArray.push({ x, y });
+  }
+  return circleArray;
+}
 export const animateCanvas = (
   canvas: HTMLCanvasElement,
   ctx: CanvasRenderingContext2D
@@ -22,5 +41,5 @@ export const animateCanvas = (
     Array.prototype.slice.call(panelHeaders);
   const heroHeaders = document.querySelectorAll(".hero__header");
   let heroHeadersArray: HTMLElement[] = Array.prototype.slice.call(heroHeaders);
- 
-}
+  window.addEventListener("resize", () => setDimensions(canvas));
+};
