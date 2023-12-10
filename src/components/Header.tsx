b8c018b924dd76977ec4a73142b15285867fafcb
NikKowPHP <nik.kow@outlook.com>
2023-12-10 08:32:35 +0100

    feat(Header): Create dedicated file for Header component
    Establish a separate file specifically for the Header component. This addition isolates the Header component's code into its own file, promoting better organization and maintainability within the project structure.

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+  const Header = (): JSX.Element => (
+    <>
+      <section>
+        <div className="hero-text">
+          <div>
+            <span>
+              <h1 className="hero__header">2023</h1>
+            </span>
+          </div>
+          <div>
+            <span>
+              <h1 className="hero__header">PORTFOLIO</h1>
+            </span>
+          </div>
+          <div>
+            <span>
+              <h1 className="hero__header">MIKITA KAVALIOU</h1>
+            </span>
+          </div>
+        </div>
+        <div className="panel__container">
+          <div className="panel">
+            <div className="text__wrap reverse">
+              <h1>PHP DEVELOPER</h1>
+            </div>
+          </div>
+          <div className="panel">
+            <div className="text__wrap forward">
+              <h1>LARAVEL</h1>
+            </div>
+          </div>
+          <div className="panel">
+            <div className="text__wrap reverse">
+              <h1>JAVASCRIPT</h1>
+            </div>
+          </div>
+          <div className="panel">
+            <div className="text__wrap forward">
+              <h1>REACT</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <canvas id="headerCanvas"></canvas>
+    </>
+  );
+	export default Header;
