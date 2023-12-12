a98ba6520e1a2a9f10ae3358b2ddefcc41a70036
NikKowPHP <nik.kow@outlook.com>
2023-12-12 11:45:26 +0100

    feat(router): Integrate App wrapper component into router
    Update the router configuration to include the App wrapper component. This integration enhances the application structure by encapsulating the entire app within a wrapper, allowing for better control and management of the application's layout and components.

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -5,27 +5,38 @@ import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import App from "../App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Welcome /> ,
-  },
-  {
-    path: "/about",
-    element: <About/> ,
-  },
-  {
-    path: "/contact",
-    element: <Contact/> ,
-  },
-  {
-    path: "/projects",
-    element: <Projects/> ,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/welcome" />,
+      },
+      {
+        path: "/welcome",
+        element: <Welcome />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
