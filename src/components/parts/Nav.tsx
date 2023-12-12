8bbc5f4fd7b191bedd48599acaa3264dea002127
NikKowPHP <nik.kow@outlook.com>
2023-12-12 09:39:32 +0100

    feat(nav): Implement animation for navigation list

--- src/components/parts/Nav.tsx
+++ src/components/parts/Nav.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const Nav: React.FC = () => {
-	return (
-		<div className='absolute top-0  w-full  bg-transparent  z-10'>
-			<nav className='max-w-4x1 mx-auto'>
-				<ul className='flex justify-around  list-none text-white'>
-					<li className='p-4'>About </li>
-					<li className='p-4'>Projects</li>
-					<li className='p-4'>Contact</li>
-				</ul>
+  useEffect(() => {
+    const animateNavLinks = () => {
+      const navLinks = document.querySelectorAll<HTMLElement>(".nav-link");
+      const navLinksArray: HTMLElement[] = Array.from(navLinks);
 
-			</nav>
+      navLinksArray.forEach((link, idx) => {
+        setTimeout(() => {
+          link.style.transform = "translateY(0)";
+        }, 3000 + idx * 400);
+      });
+    };
+    animateNavLinks();
+  }, []);
 
-		</div>
-	)
-}
+  return (
+    <div className="absolute top-0  w-full  bg-transparent  z-10">
+      <nav className="max-w-4x1 mx-auto">
+        <ul className="flex justify-around list-none text-white">
+          <li className="nav-link p-4 transition-transform transform -translate-y-full duration-1000  hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
+            About
+          </li>
+          <li className="nav-link p-4 transition-transform transform  -translate-y-full duration-1000 hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
+            Projects
+          </li>
+          <li className="nav-link p-4 transition-transform transform  -translate-y-full duration-1000 hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
+            Contact
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Main
+export default Nav;
