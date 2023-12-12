b275bfdafa41ce20b007882c575c63a186597a6a
NikKowPHP <nik.kow@outlook.com>
2023-12-12 11:18:41 +0100

    refactor: Extract main app code into dedicated generic component
    Reorganize the code within the main app component by extracting it into a dedicated generic component. This extraction aims to create a reusable and more focused component, enhancing modularity and maintainability within the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import { initializeCanvas} from "./components/CanvasAnimation";
+import React from "react";
+import "styles/App.css";
+import Nav from "./components/parts/Nav";
+
+
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const canvas: HTMLCanvasElement = document.querySelector("#headerCanvas")!;
-    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
-    initializeCanvas(canvas, ctx);
-  }, []);
-
   return (
     <>
-      <Header />
-      <canvas id="headerCanvas"></canvas>
+      <Nav />
     </>
   );
 };
---
+++ src/components/pages/Welcome.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "../parts/Header";
+import { initializeCanvas} from "../canvas/CanvasAnimation";
+
+const Welcome: React.FC = () => {
+  useEffect(() => {
+    const canvas: HTMLCanvasElement = document.querySelector("#headerCanvas")!;
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+    initializeCanvas(canvas, ctx);
+  }, []);
+
+
+
+  return (
+    <>
+      <Header />
+      <canvas id="headerCanvas"></canvas>
+    </>
+  );
+};
+
+export default Welcome;
