6f58d377a3520284323480ddb74922509017aed2
NikKowPHP <nik.kow@outlook.com>
2023-12-13 09:57:20 +0100

    feat(nav): Implement navigation animation completion before link navigation
    Introduce a feature that ensures navigation to the destination link occurs only after the completion of the navigation animation. This addition enhances user experience by synchronizing link navigation with the completion of the navigation animation.

--- src/components/parts/Nav.tsx
+++ src/components/parts/Nav.tsx
@@ -1,36 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Nav: React.FC = () => {
-  useEffect(() => {
-    const animateNavLinks = () => {
-      const navLinks = document.querySelectorAll<HTMLElement>(".nav-link");
-      const navLinksArray: HTMLElement[] = Array.from(navLinks);
+  const navigate = useNavigate();
+  const [navLinks, setNavLinks] = useState<HTMLElement[]>([]);
 
-      navLinksArray.forEach((link, idx) => {
+  const animateNavLinks = () => {
+    navLinks.forEach((link, idx) => {
+      setTimeout(() => {
+        link.style.transform = "translateY(0)";
+      }, 3000 + idx * 400);
+    });
+  };
+  const animateNavLinksReverse = (): Promise<boolean> => {
+    return new Promise((resolve) => {
+      let completedAnimations = 0;
+      navLinks.forEach((link, idx) => {
         setTimeout(() => {
-          link.style.transform = "translateY(0)";
-        }, 3000 + idx * 400);
+          link.style.transform = "translateY(-100%)";
+          completedAnimations++;
+          if (completedAnimations === navLinks.length) {
+            resolve(true)
+          }
+        }, idx * 400);
       });
-    };
+    });
+  };
+  const handleLinkClick = async (navLink: string) => {
+    await animateNavLinksReverse();
+    navigate(navLink);
+  };
+  useEffect(() => {
     animateNavLinks();
+  }, [navLinks]);
+
+  useEffect(() => {
+    const navLinks = document.querySelectorAll<HTMLElement>(".nav-link");
+    const navLinksArray: HTMLElement[] = Array.from(navLinks);
+    setNavLinks(navLinksArray);
   }, []);
 
   return (
-    <div className="absolute top-0  w-full  bg-transparent  z-10">
-      <nav className="max-w-4x1 mx-auto">
-        <ul className="flex justify-around list-none text-white">
-          <li className="nav-link p-4 transition-transform transform -translate-y-full duration-1000  hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
-            About
+    <nav className=" absolute top-0  w-full  bg-transparent  z-10">
+      <div className="max-w-4x1 mx-auto">
+        <ul className="flex justify-around list-none text-white font-semibold">
+          <li className="nav-link p-4 transition-transform transform -translate-y-full duration-1000 hover:text-lg font-semibold  ">
+            <span onClick={() => handleLinkClick("/welcome")}>Home</span>
           </li>
-          <li className="nav-link p-4 transition-transform transform  -translate-y-full duration-1000 hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
-            Projects
+          <li className="nav-link p-4 transition-transform transform -translate-y-full duration-1000 hover:text-lg font-semibold  ">
+            <span onClick={() => handleLinkClick("/about")}>About</span>
           </li>
-          <li className="nav-link p-4 transition-transform transform  -translate-y-full duration-1000 hover:scale-110 hover:bg-gray-700 hover:text-blue-500">
-            Contact
+          <li className="nav-link p-4 transition-transform transform  -translate-y-full duration-1000 hover:scale-110 font-semibold">
+            <span onClick={() => handleLinkClick("/projects")}>Projects</span>
+          </li>
+          <li className="nav-link hover:scale-110 p-4 transition-transform transform  -translate-y-full duration-1000 font-semibold ">
+            <span onClick={() => handleLinkClick("/contact")}>Contact</span>
           </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
