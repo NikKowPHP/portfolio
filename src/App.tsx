674afe2e72b0eab07b6bb9d1bfc23d0ae10e2e30
NikKowPHP <nik.kow@outlook.com>
2023-12-13 11:57:46 +0100

    feat(app): Implement app-wide RedirectContextProvider for redirect state
    Wrap the entire application with the RedirectContextProvider to provide access to the redirect state across the app. This implementation ensures that the redirect state is available globally, facilitating its usage and management throughout the application.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import "./styles/app.css";
 import Nav from "./components/parts/Nav";
 import { Outlet } from "react-router-dom";
-
-
+import { RedirectContextProvider } from "./components/contexts/RedirectContext";
 
 const App: React.FC = () => {
   return (
-    <>
+    <RedirectContextProvider>
       <Nav />
       <Outlet />
-    </>
+    </RedirectContextProvider>
   );
 };
 
