ab1efe5b4164011abbd145eef2fa7737d62243fe
NikKowPHP <nik.kow@outlook.com>
2023-12-14 10:04:07 +0100

    feat(About): Introduce typewriting effect in About page content
    Implement typewriting logic within the "About" page content. This addition introduces a dynamic typing effect to the content on the About page, enhancing its visual appeal and engagement.

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import TextTypingEffect from "../TextTypingEffect";
 
 const About: React.FC = () => {
+  const texts = [
+    "About Me: I'm a dedicated and enthusiastic junior ",
+    "developer with a deep passion for crafting web applications using PHP Laravel and JavaScript React.",
+    "💼 Seeking Job Opportunities",
+    "My Strengths:",
+    "💡 Innovative Problem Solver: I enjoy tackling complex challenges and finding creative solutions.",
+    "I firmly believe that the tech world is ever-evolving, and I'm committed to continuous learning.",
+  ];
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [isTypingComplete, setIsTypingComplete] = useState<boolean>(false);
 
-	return (
-		<div className='text-white'>
-			About page
-		</div>
-	);
-}
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        if (!isTypingComplete) return prevIndex;
+        if (prevIndex < texts.length - 1) {
+          setIsTypingComplete(false);
+          return prevIndex + 1;
+        } else {
+          clearInterval(interval);
+          return prevIndex;
+        }
+      });
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [texts.length, isTypingComplete]);
+
+  return (
+    <div className="container mx-auto max-w-3x1 mt-20 ">
+      <div className="mt-20  text-white">
+        {texts.slice(0, currentIndex + 1).map((text, index) => (
+          <div key={index}>
+            <div className="p-4">
+              <TextTypingEffect
+                text={text}
+                durationInMs={30}
+                onComplete={() => setIsTypingComplete(true)}
+              />{" "}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default About;
