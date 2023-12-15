e079ca7a7d6d2851f792076bc1813f663acf8cda
NikKowPHP <nik.kow@outlook.com>
2023-12-15 09:15:16 +0100

    feat(TextTypingEffect): Implement blinking cursor functionality
    Introduce the functionality of a blinking cursor within the TextTypingEffect component. This addition creates a blinking cursor effect, enhancing the typewriting animation by simulating a cursor for a more authentic typing experience.

--- src/components/TextTypingEffect.tsx
+++ src/components/TextTypingEffect.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "../styles/typewritter.css";
 
 interface TextTypingEffectProps {
   text: string;
@@ -11,8 +12,11 @@ const TextTypingEffect: React.FC<TextTypingEffectProps> = ({
   durationInMs,
   onComplete,
 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const [displayText, setDisplayText] = useState<string>("");
   const [currentPosition, setCurrentPosition] = useState<number>(0);
+  const [isCursorVisible, setIsCursorVisible] = useState<boolean>(true);
+  const [isTypingComplete, setIsTypingComplete] = useState<boolean>(false);
 
   useEffect(() => {
     const textLength = text.length;
@@ -21,16 +25,33 @@ const TextTypingEffect: React.FC<TextTypingEffectProps> = ({
         if (currentPos === textLength) {
           clearInterval(interval);
           onComplete();
+          setIsTypingComplete(true);
           return currentPos;
         }
         setDisplayText(text.substring(0, currentPos + 1));
-				return currentPos + 1;
+        return currentPos + 1;
       });
     }, durationInMs);
+
+
+    const cursorInterval = setInterval(() => {
+      if (!isTypingComplete) {
+        setIsCursorVisible((prevVisibility) => !prevVisibility);
+      } else {
+        clearInterval(cursorInterval);
+        setIsCursorVisible(false);
+      }
+    }, 500);
     return () => {
       clearInterval(interval);
+      clearInterval(cursorInterval);
     };
   }, [text]);
-  return displayText.substring(0, currentPosition);
+  return (
+    <div>
+      <span ref={containerRef} className="type-writter">{displayText.substring(0, currentPosition)}</span>
+      {isCursorVisible && !isTypingComplete &&  <span className="cursor">|</span>}
+    </div>
+  );
 };
 export default TextTypingEffect;
