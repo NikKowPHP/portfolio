47c063f8bfeaf983487dfed74b46591ed37a8ea6
NikKowPHP <nik.kow@outlook.com>
2023-12-17 10:20:54 +0100

    feat(darkMode): Set dark mode as default theme
    Switch the application's default theme to dark mode. This change establishes dark mode as the primary theme, enhancing user preference options and providing a different visual experience by default.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        whiteBlue: 'rgba(240,248,255)',
-      }
+        whiteBlue: "rgba(240,248,255)",
+      },
     },
   },
   plugins: [],
-}
-
+};
