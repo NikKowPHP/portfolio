8933cf56ffad8017b2fe17479f41a61bc7564fd8
NikKowPHP <nik.kow@outlook.com>
2023-12-17 09:36:09 +0100

    feat(projects): Sequentially render blocks with typewriting effect
    Introduce functionality to sequentially render blocks within the "projects" section using a typewriting effect. This addition allows for a gradual and sequential display of content blocks, enhancing the presentation and engagement within the projects section.

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import TextTypingEffect from "../TextTypingEffect";
 
- const Projects: React.FC = () => {
+const Projects: React.FC = () => {
+  const [previosTextComplete, setPreviosTextComplete] =
+    useState<boolean>(false);
+  const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
+	const [completedBlocks, setCompletedBlocks] = useState<number[]>([0]);
 
-	return (
-		<div>
-			projects page
-		</div>
-	);
-}
+
+
+  const handleTypingComplete = (index: number) => {
+		setCompletedBlocks((prevBlocks) => [...prevBlocks, index + 1])
+    setPreviosTextComplete(true);
+  };
+
+
+
+  const texts = ["/projects", "Selected projects i've created"];
+
+
+
+
+  useEffect(() => {
+    if (previosTextComplete) {
+      setTimeout(() => {
+        setCurrentTextIndex(currentTextIndex + 1);
+        setPreviosTextComplete(false);
+      }, 1000);
+    }
+  }, [previosTextComplete, currentTextIndex]);
+
+  return (
+    <div className="container text-white p-20">
+      {completedBlocks.includes(0) && (
+        <h1>
+          <TextTypingEffect
+            text={texts[0]}
+            durationInMs={100}
+            onComplete={() => handleTypingComplete(0)}
+          />
+        </h1>
+      )}
+      {completedBlocks.includes(1) && (
+        <h3>
+          <TextTypingEffect
+            text={texts[1]}
+            durationInMs={100}
+            onComplete={() => handleTypingComplete(1)}
+          />
+        </h3>
+      )}
+    </div>
+  );
+};
 export default Projects;
