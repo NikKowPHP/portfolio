bd279a6473bb48f147934d7aec9fe116adb48c31
NikKowPHP <nik.kow@outlook.com>
2023-12-17 09:52:31 +0100

    feat(projects): Center header during sequential rendering
    Implement sequential rendering in the "projects" section while ensuring the header is centered. This addition centers the header within the section, maintaining visual consistency and alignment during the sequential display of content, enhancing overall readability and presentation.

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -5,21 +5,14 @@ const Projects: React.FC = () => {
   const [previosTextComplete, setPreviosTextComplete] =
     useState<boolean>(false);
   const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
-	const [completedBlocks, setCompletedBlocks] = useState<number[]>([0]);
-
-
+  const [completedBlocks, setCompletedBlocks] = useState<number[]>([0]);
 
   const handleTypingComplete = (index: number) => {
-		setCompletedBlocks((prevBlocks) => [...prevBlocks, index + 1])
+    setCompletedBlocks((prevBlocks) => [...prevBlocks, index + 1]);
     setPreviosTextComplete(true);
   };
 
-
-
   const texts = ["/projects", "Selected projects i've created"];
-
-
-
 
   useEffect(() => {
     if (previosTextComplete) {
@@ -30,26 +23,35 @@ const Projects: React.FC = () => {
     }
   }, [previosTextComplete, currentTextIndex]);
 
+
+	const renderHeader = () => {
+		return (
+      <header className="projects-header__title flex flex-col items-center justify-center w-full">
+        {completedBlocks.includes(0) && (
+          <h1 className="text-center flex-1">
+            <TextTypingEffect
+              text={texts[0]}
+              durationInMs={100}
+              onComplete={() => handleTypingComplete(0)}
+            />
+          </h1>
+        )}
+        {completedBlocks.includes(1) && (
+          <h3 className="text-center w-full flex-1">
+            <TextTypingEffect
+              text={texts[1]}
+              durationInMs={100}
+              onComplete={() => handleTypingComplete(1)}
+            />
+          </h3>
+        )}
+      </header>
+		)
+	}
+
   return (
-    <div className="container text-white p-20">
-      {completedBlocks.includes(0) && (
-        <h1>
-          <TextTypingEffect
-            text={texts[0]}
-            durationInMs={100}
-            onComplete={() => handleTypingComplete(0)}
-          />
-        </h1>
-      )}
-      {completedBlocks.includes(1) && (
-        <h3>
-          <TextTypingEffect
-            text={texts[1]}
-            durationInMs={100}
-            onComplete={() => handleTypingComplete(1)}
-          />
-        </h3>
-      )}
+    <div className="text-white mt-20 w-full">
+			{renderHeader()}
     </div>
   );
 };
