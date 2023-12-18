3bd385fd598a96630ac120ad7e08364127edb7e2
NikKowPHP <nik.kow@outlook.com>
2023-12-18 08:37:33 +0100

    feat(Project): Introduce Project component
    Implement the Project component, establishing a dedicated module for individual projects. This addition creates a reusable component specifically designed to showcase and manage project-specific details within the application.

---
+++ src/components/pages/Project.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+interface ProjectProps {
+  image: string;
+  title: string;
+  description: string;
+  link: string;
+  onClick: (link: string) => void;
+}
+
+const Card: React.FC<ProjectProps> = ({
+  image,
+  title,
+  description,
+  link,
+  onClick,
+}) => {
+  const alt = image.slice(0, -4);
+
+
+
+  return (
+    <div
+      style={{ transform: "translateY(250%)" }}
+      className="card transition-transform transform  duration-500 m-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <div className="flex justify-center mt-5" onClick={() => onClick(link)}>
+        <img
+          className="rounded-t-lg rounded-b-lg w-1/2  h-auto"
+          src={`/src/assets/images/${image}`}
+          alt={alt}
+        />
+      </div>
+      <div className="p-5">
+        <div onClick={() => onClick(link)}>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+            {title}
+          </h5>
+        </div>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Card;
