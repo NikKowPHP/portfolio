506bc071c4798d69ed72225fb4db2dde36cf6040
NikKowPHP <nik.kow@outlook.com>
2023-12-18 08:33:10 +0100

    feat(Card): Implement callback function with link as a prop
    Introduce a callback function within the Card component that utilizes a link provided as a prop. This addition allows the Card component to handle navigation or perform specific actions when triggered via the provided link, enhancing its functionality and flexibility.

--- src/components/parts/Card.tsx
+++ src/components/parts/Card.tsx
@@ -6,26 +6,35 @@ interface CardProps {
   title: string;
   description: string;
   link: string;
+  onClick: (link: string) => void;
 }
 
-const Card: React.FC<CardProps> = ({ image, title, description, link }) => {
-
+const Card: React.FC<CardProps> = ({
+  image,
+  title,
+  description,
+  link,
+  onClick,
+}) => {
   const alt = image.slice(0, -4);
   return (
-    <div style={{transform: 'translateY(250%)'}} className="card transition-transform transform  duration-500 m-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <Link className="flex justify-center mt-5" to={`/projects/${link}`}>
+    <div
+      style={{ transform: "translateY(250%)" }}
+      className="card transition-transform transform  duration-500 m-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <div className="flex justify-center mt-5" onClick={() => onClick(link)}>
         <img
           className="rounded-t-lg rounded-b-lg w-1/2  h-auto"
           src={`/src/assets/images/${image}`}
           alt={alt}
         />
-      </Link>
+      </div>
       <div className="p-5">
-        <Link to={`/projects/${link}`}>
+        <div onClick={() => onClick(link)}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
             {title}
           </h5>
-        </Link>
+        </div>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
