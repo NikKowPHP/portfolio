c6f57fed53c4f30f9c88d3b2d19d96311d4e5adb
NikKowPHP <nik.kow@outlook.com>
2023-12-20 11:42:59 +0100

    feat(Project): Replace explicit carousel with Carousel component, adjust dimensions
    Refactor the Project component to utilize the Carousel component instead of the explicit carousel implementation. Additionally, modify the carousel dimensions within the Project to ensure optimal display and alignment of the image content.

--- src/components/pages/Project.tsx
+++ src/components/pages/Project.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useEffect, useRef, useState } from "react";
-import { json, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import TextTypingEffect from "../TextTypingEffect";
+import Carousel from "../parts/Carousel";
 
 interface ProjectData {
   title: string;
@@ -27,7 +28,7 @@ const Project: React.FC = () => {
     null
   );
   const [isTypingComplete, setIsTypingComplete] = useState<boolean>(false);
-  const [currentSlide, setCurrentSlide] = useState(0);
+
 
   const [data, setData] = useState<ProjectData[]>([]);
 
@@ -71,112 +72,22 @@ const Project: React.FC = () => {
     }
   }, [isTypingComplete]);
 
-  const handlePrev = () => {
-    setCurrentSlide((prevSlide) =>
-      selectedProject && prevSlide === 0
-        ? selectedProject?.images.length - 1
-        : prevSlide - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentSlide((prevSlide) =>
-      selectedProject && prevSlide === selectedProject?.images.length - 1
-        ? 0
-        : prevSlide + 1
-    );
-  };
 
   const renderCorousel: () => ReactElement = () => {
     return (
-      <div
-        id="default-carousel"
-        className="corousel relative w-full py-16 m-auto px-4 group"
-      >
-        <div className="relative max-w-[1400px]  overflow-hidden rounded-lg md:h-96">
-          {selectedProject?.images.map((image, index) => (
-            <div
-              key={index}
-              className={`duration-700 ease-in-out ${
-                index === currentSlide ? "block" : "hidden"
-              }`}
-            >
+      <div className="flex justify-center  ">
+        <div className=" max-w-2x1 lg:max-w-[1200px] md:max-w-[1000px]  sm:max-w-full px-4 mb-5 ">
+          <Carousel autoSlide={true}>
+            {selectedProject?.images.map((image, index) => (
               <img
+                key={index}
                 src={`/src/assets/images/${image}`}
-                className="absolute h-full block  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 alt={image}
+                className="w-full h-auto"
               />
-            </div>
-          ))}
+            ))}
+          </Carousel>
         </div>
-
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          {selectedProject?.images.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              className={`w-3 h-3 rounded-full ${
-                currentSlide === index ? "bg-gray-600" : "bg-gray-300"
-              }`}
-              aria-current={currentSlide === index}
-              aria-label={`Slide ${index + 1}`}
-              data-carousel-slide-to={index}
-              onClick={() => setCurrentSlide(index)}
-            ></button>
-          ))}
-        </div>
-
-        <button
-          type="button"
-          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
-          onClick={handlePrev}
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-500/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-
-        <button
-          type="button"
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
-          onClick={handleNext}
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/50 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
       </div>
     );
   };
