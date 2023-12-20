91844301960532f10e21cc3ebec6a088fa44957f
NikKowPHP <nik.kow@outlook.com>
2023-12-20 09:10:35 +0100

    feat(Project): Implement image carousel functionality
    Introduce an image carousel feature within the Project component. This addition enables the display of multiple project images in a carousel format, allowing users to navigate through a collection of project visuals for a comprehensive viewing experience.

--- src/components/pages/Project.tsx
+++ src/components/pages/Project.tsx
@@ -27,6 +27,7 @@ const Project: React.FC = () => {
     null
   );
   const [isTypingComplete, setIsTypingComplete] = useState<boolean>(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const [data, setData] = useState<ProjectData[]>([]);
 
@@ -70,26 +71,112 @@ const Project: React.FC = () => {
     }
   }, [isTypingComplete]);
 
+  const handlePrev = () => {
+    setCurrentSlide((prevSlide) =>
+      selectedProject && prevSlide === 0
+        ? selectedProject?.images.length - 1
+        : prevSlide - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentSlide((prevSlide) =>
+      selectedProject && prevSlide === selectedProject?.images.length - 1
+        ? 0
+        : prevSlide + 1
+    );
+  };
 
   const renderCorousel: () => ReactElement = () => {
     return (
-      <div className="relative mb-5 corousel">
-        <div className="overflow-hidden ">
-          <div className="flex">
-            {selectedProject?.images.map((image, index) => (
-              <div
-                key={index}
-                className={`w-full h-full ${
-                  index === 0 ? "ml-0" : "-ml-96"
-                } transition-transform ease-in-out duration-500 transform ${
-                  index === 0 ? "translate-x-0" : "translate-x-full"
-                }`}
-              >
-                <img src={`/src/assets/images/${image}`} />
-              </div>
-            ))}
-          </div>
+      <div
+        id="default-carousel"
+        className="corousel relative w-full py-16 m-auto px-4 group"
+      >
+        <div className="relative max-w-[1400px]  overflow-hidden rounded-lg md:h-96">
+          {selectedProject?.images.map((image, index) => (
+            <div
+              key={index}
+              className={`duration-700 ease-in-out ${
+                index === currentSlide ? "block" : "hidden"
+              }`}
+            >
+              <img
+                src={`/src/assets/images/${image}`}
+                className="absolute h-full block  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt={image}
+              />
+            </div>
+          ))}
         </div>
+
+        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          {selectedProject?.images.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`w-3 h-3 rounded-full ${
+                currentSlide === index ? "bg-gray-600" : "bg-gray-300"
+              }`}
+              aria-current={currentSlide === index}
+              aria-label={`Slide ${index + 1}`}
+              data-carousel-slide-to={index}
+              onClick={() => setCurrentSlide(index)}
+            ></button>
+          ))}
+        </div>
+
+        <button
+          type="button"
+          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+          onClick={handlePrev}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-500/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+
+        <button
+          type="button"
+          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+          onClick={handleNext}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/50 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
       </div>
     );
   };
