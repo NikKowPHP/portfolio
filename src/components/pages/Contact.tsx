8d0d65f027430efb812c78a0c3e01743cd0a1afe
NikKowPHP <nik.kow@outlook.com>
2023-12-21 11:14:25 +0100

    feat(Contact): Integrate email sending functionality using EmailJS library
    Implement the functionality to send emails within the Contact component by integrating the EmailJS library. This addition allows seamless email transmission, enhancing user interaction and enabling efficient communication through the Contact feature.

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+
+interface Data {
+
+}
 
 const Contact: React.FC = () => {
+	const form = useRef();
 
-	return (
-		<div>
-		contact
-		</div>
-	);
-}
-export default Contact
+
+
+	const sendEmail = (e: Event) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_2a5v99a', 'template_eb3m4nu', form.current, 'MBv-C3sqmGh53tFYY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  const renderForm = () => {
+    return (
+      <form className="flex flex-col " ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input className="text-black" type="text" name="user_name" />
+        <label>Email</label>
+        <input className="text-black" type="email" name="user_email" />
+        <label>Message</label>
+        <textarea className="text-black" name="message" />
+        <input className="mt-2 bg-gray-300" type="submit" value="Send" />
+      </form>
+    );
+  };
+  return <div>
+		<div className="mt-20 text-white ">{renderForm()}</div>
+	</div>;
+};
+export default Contact;
