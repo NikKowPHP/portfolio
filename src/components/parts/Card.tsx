923e3a5ed7b15ad53f4db6fc38f8e17a8ae157c2
NikKowPHP <nik.kow@outlook.com>
2023-12-21 10:31:17 +0100

    refactor(Card): Revise Card component with updated class structure
    Adjust the Card component by implementing new class structures to improve readability and maintainability. This refactoring includes minor adjustments to the existing codebase, ensuring better organization and clarity within the Card component.

--- src/components/parts/Card.tsx
+++ src/components/parts/Card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 interface CardProps {
   image: string;
@@ -20,16 +19,16 @@ const Card: React.FC<CardProps> = ({
   return (
     <div
       style={{ transform: "translateY(250%)" }}
-      className="card transition-transform transform  duration-500 m-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      className="card transition-transform transform duration-500 m-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transition duration-500 ease-in-out hover:duration-500 cursor-pointer "
     >
-      <div className="flex justify-center mt-5" onClick={() => onClick(link)}>
+      <div className="flex justify-center mt-5 " onClick={() => onClick(link)}>
         <img
-          className="rounded-t-lg rounded-b-lg w-1/2  h-auto"
+          className="rounded-t-lg rounded-b-lg w-1/2  h-1/2"
           src={`./src/assets/images/${image}`}
           alt={alt}
         />
       </div>
-      <div className="p-5">
+      <div className="p-5 text-center">
         <div onClick={() => onClick(link)}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
             {title}
