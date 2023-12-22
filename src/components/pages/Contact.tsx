3d1bb86b65e88ea895e30b21ada477c1ffc287f4
NikKowPHP <nik.kow@outlook.com>
2023-12-22 16:23:30 +0100

    feat(Contact): Add sequential typewriting and form animations
    Implement sequential typewriting for content appearance and introduce animations for the form within the Contact component. This addition enhances the visual presentation by displaying content sequentially and animating form elements, improving user engagement and interaction.

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,9 +1,39 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
 import TextTypingEffect from "../TextTypingEffect";
 
 const Contact: React.FC = () => {
   const form = useRef<HTMLFormElement>(null);
+
+  const [previosTextComplete, setPreviosTextComplete] =
+    useState<boolean>(false);
+  const [completedBlocks, setCompletedBlocks] = useState<number[]>([0]);
+  const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (previosTextComplete) {
+      setTimeout(() => {
+        setCurrentTextIndex(currentTextIndex + 1);
+        setPreviosTextComplete(false);
+      }, 1000);
+    }
+  }, [previosTextComplete, currentTextIndex]);
+  useEffect(() => {
+    const formCurrent = form.current;
+    if(formCurrent) {
+    setTimeout(() => {
+      formCurrent.style.transform = 'translateY(0)';
+
+    }, 3000)
+
+    }
+
+  }, [form])
+
+  const handleTypingComplete = (index: number) => {
+    setCompletedBlocks((prevBlocks) => [...prevBlocks, index + 1]);
+    setPreviosTextComplete(true);
+  };
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -26,11 +56,10 @@ const Contact: React.FC = () => {
         );
     }
   };
-  const handleTypingComplete = () => {};
 
   const renderForm = () => {
     return (
-      <form className="flex flex-col " ref={form} onSubmit={sendEmail}>
+      <form className="flex flex-col translate-y-[250%] transition-all ease-out duration-1000 " ref={form} onSubmit={sendEmail}>
         <input
           className=" bg-transparent border border-solid border-whiteBlue border-opacity-20 hover:border-opacity-40 focus:border-opacity-60 transition-all duration-300 mb-4 p-4 rounded-lg"
           placeholder="Name"
@@ -59,24 +88,35 @@ const Contact: React.FC = () => {
   return (
     <div>
       <div className="mt-16 text-white text-center form-header max-w-[1200px] m-auto">
+        {completedBlocks.includes(0) && (
         <h1 className=" lg:text-9xl ">
           <TextTypingEffect
             durationInMs={100}
-            onComplete={handleTypingComplete}
+            onComplete={()=>handleTypingComplete(0)}
             text="/Contact."
           />
         </h1>
+        )}
         <div>
-          <div className="font-bold">
+          {completedBlocks.includes(1) && (
             <TextTypingEffect
               durationInMs={50}
-              onComplete={handleTypingComplete}
-              text="Get in touch or shoot me an email directly on  nik.kow@outlook.com"
+              onComplete={() => handleTypingComplete(1)}
+              text="Get in touch or shoot me an email directly on  "
             />
-          </div>
+          )}
+          {completedBlocks.includes(2) && (
+          <div className="font-bold text-xl">
+            <TextTypingEffect
+              durationInMs={50}
+              onComplete={() => handleTypingComplete(1)}
+              text="nik.kow@outlook.com"
+            />
+          </div >
+          )}
         </div>
       </div>
-      <div className="mt-20 text-white m-auto max-w-[570px]">
+      <div className="mt-20  overflow-hidden text-white m-auto max-w-[570px]">
         {renderForm()}
       </div>
     </div>
