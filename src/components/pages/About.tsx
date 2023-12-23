3f10e5a5704d74a4cc6ce64857c52c85cda68aeb
NikKowPHP <nik.kow@outlook.com>
2023-12-23 18:21:51 +0100

    feat(projects): Integrate panels component into About feature

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import TextTypingEffect from "../TextTypingEffect";
 import { aboutMe } from "../../assets/infoContent/aboutMe";
+import Panels from "../parts/Panels";
 
 const About: React.FC = () => {
   const texts = aboutMe;
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [isTypingComplete, setIsTypingComplete] = useState<boolean>(false);
-
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,21 +26,24 @@ const About: React.FC = () => {
   }, [texts.length, isTypingComplete]);
 
   return (
-    <div className="container mx-auto max-w-3x1 mt-20 ">
-      <div className="mt-20  text-white">
-        {texts.slice(0, currentIndex + 1).map((text, index) => (
-          <div key={index}>
-            <div className="p-4">
-              <TextTypingEffect
-                text={text}
-                durationInMs={30}
-                onComplete={() => setIsTypingComplete(true)}
-              />{" "}
+    <>
+    <Panels />
+      <div className="relative z-10 container mx-auto max-w-3x1 mt-20 ">
+        <div className="mt-20  text-white">
+          {texts.slice(0, currentIndex + 1).map((text, index) => (
+            <div key={index}>
+              <div className="p-4">
+                <TextTypingEffect
+                  text={text}
+                  durationInMs={30}
+                  onComplete={() => setIsTypingComplete(true)}
+                />{" "}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
