135cf960fd3117c84ee039cf482fa5d963d75b18
NikKowPHP <nik.kow@outlook.com>
2023-12-24 10:16:14 +0100

    feat(footer): Implement useLinkAnimation hook for link interactions
    Update the Footer component to utilize the useLinkAnimation hook for managing link interactions. This enhancement enables the handling of link clicks within the hook, ensuring consistent and animated behavior for footer links across the application.

--- src/components/parts/Footer.tsx
+++ src/components/parts/Footer.tsx
@@ -1,62 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useRedirectContext } from "../contexts/RedirectContext";
+import React from "react";
 import { svg } from "../../assets/images/svg/svg";
+import { useLinkAnimation } from "../hooks/useLinkAnimation";
 
 const Footer: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [footerLinks, setFooterLinks] = useState<HTMLElement[]>([]);
-  const { setIsRedirecting, isRedirecting } = useRedirectContext();
-
-  const animateFooter = () => {
-    footerLinks.forEach((link, idx) => {
-      setTimeout(() => {
-        link.style.transform = "translateX(0)";
-      }, 3000 + idx * 400);
-    });
-  };
-  const animateNavLinksReverse = (): Promise<boolean> => {
-    return new Promise((resolve) => {
-      let completedAnimations = 0;
-      footerLinks.forEach((link, idx) => {
-        setTimeout(() => {
-          link.style.transform = "translateY(-100%)";
-          completedAnimations++;
-          if (completedAnimations === footerLinks.length) {
-            resolve(true);
-          }
-        }, idx * 400);
-      });
-    });
-  };
-  const handleLinkClick = async (navLink: string) => {
-    setIsRedirecting(true);
-    await animateNavLinksReverse();
-    navigate(navLink);
-    setIsRedirecting(false);
-  };
-  useEffect(() => {
-    animateFooter();
-  }, [footerLinks, location.pathname]);
-
-  useEffect(() => {
-    const footerLinks = document.querySelectorAll<HTMLElement>(".footer-link");
-    const footerLinksArray: HTMLElement[] = Array.from(footerLinks);
-    setFooterLinks(footerLinksArray);
-  }, []);
+  const { handleLinkClick } = useLinkAnimation(
+    [".footer-link", ".nav-link"],
+    400,
+    400
+  );
 
   return (
     <footer className="absolute bottom-0  w-full bg-transparent text-white z-10">
       <div className=" container mx-auto max-w-3xl ">
         <ul className="flex justify-around overflow-hidden">
-          <li className="footer-link transition-transform transform translate-y-full duration-1000">
-            <a className="p-4 block" href="in/mikita-kavaliou-390b62236">
+          <li onClick={()=> handleLinkClick("https://www.linkedin.com/in/mikita-kavaliou-390b62236/", true)} className="footer-link transition-transform transform translate-y-full duration-1000 cursor-pointer">
+            <span className="p-4 block" >
               {svg.linkedIn}
-            </a>
+            </span>
           </li>
-          <li className="footer-link transition-transform transform translate-y-full duration-1000 ">
-            <a className="block  p-4" href="https://github.com/NikKowPHP">{svg.github}</a>
+          <li onClick={() => handleLinkClick("https://github.com/NikKowPHP", true)} className="footer-link transition-transform transform translate-y-full duration-1000 cursor-pointer">
+            <span className="block  p-4">
+              {svg.github}
+            </span>
           </li>
         </ul>
       </div>
