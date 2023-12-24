b43f15e4d28cb06c50418e0b97c7c695a352cbe9
NikKowPHP <nik.kow@outlook.com>
2023-12-24 09:14:44 +0100

    feat(app): Integrate Footer component into the App
    Incorporate the Footer component within the main App structure to establish consistent navigation and information presentation throughout the application. This integration ensures the presence and functionality of the footer across all app pages.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./styles/app.css";
 import Nav from "./components/parts/Nav";
 import { Outlet } from "react-router-dom";
 import { RedirectContextProvider } from "./components/contexts/RedirectContext";
+import Footer from "./components/parts/Footer";
 
 const App: React.FC = () => {
   return (
     <RedirectContextProvider>
       <Nav />
       <Outlet />
+      <Footer />
     </RedirectContextProvider>
   );
 };
