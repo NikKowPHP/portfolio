c8d3cc404ead9ca88d718d9b174c6102a25310c1
NikKowPHP <nik.kow@outlook.com>
2023-12-25 08:37:49 +0100

    feat(footer): Introduce classes prop for enhanced flexibility
    Add a classes prop to the Footer component, allowing greater flexibility in applying custom classes. This enhancement enables easier customization of the footer's appearance and behavior based on specific styling needs.

--- src/components/parts/Footer.tsx
+++ src/components/parts/Footer.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { svg } from "../../assets/images/svg/svg";
 import { useLinkAnimation } from "../hooks/useLinkAnimation";
 
-const Footer: React.FC = () => {
+interface FooterProps {
+	classes: string;
+}
+
+const Footer: React.FC<FooterProps> = ({classes}) => {
   const { handleLinkClick } = useLinkAnimation(
     [".footer-link", ".nav-link"],
     400,
@@ -10,7 +14,7 @@ const Footer: React.FC = () => {
   );
 
   return (
-    <footer className="mt-5  md:absolute  bottom-0  w-full bg-transparent text-white z-10">
+    <footer className={` ${classes} z-10  bottom-0  w-full bg-transparent text-white `}>
       <div className=" container mx-auto max-w-3xl ">
         <ul className="flex justify-around overflow-hidden">
           <li onClick={()=> handleLinkClick("https://www.linkedin.com/in/mikita-kavaliou-390b62236/", true)} className="footer-link transition-transform transform translate-y-full duration-1000 cursor-pointer">
