86ff3406469fe45b692840c68cf5244184f2f969
NikKowPHP <nik.kow@outlook.com>
2023-12-26 09:25:42 +0100

    feat(useLinkAnimation): Define animation types with typings
    Establish specific animation types with corresponding typings within the useLinkAnimation feature. This addition enhances code clarity and provides clear definitions for different animation variations.

--- src/components/hooks/useLinkAnimation.tsx
+++ src/components/hooks/useLinkAnimation.tsx
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import { useRedirectContext } from "../contexts/RedirectContext";
 import { useNavigate } from "react-router-dom";
 
+export enum AnimationType {
+  SlideIn = "slide-in",
+  SlideOut = "slide-out",
+  FadeIn = "fade-in",
+  FadeOut = "fade-out",
+}
+
 export const useLinkAnimation = (
   selectors: string[],
+  animationType: AnimationType = AnimationType.SlideIn,
   animationDelay: number = 3000,
   elementAnimationDelay: number = 400,
-  data: any|undefined = undefined
+  data: any | undefined = undefined
 ) => {
   const navigate = useNavigate();
   const [links, setLinks] = useState<HTMLElement[]>([]);
@@ -15,6 +23,20 @@ export const useLinkAnimation = (
   const animateLinks = () => {
     links.forEach((link, idx) => {
       setTimeout(() => {
+        switch (animationType) {
+          case AnimationType.SlideIn:
+            link.style.transform = "translateY(0)";
+            break;
+          case AnimationType.SlideOut:
+            link.style.transform = "translateY(-150%)";
+            break;
+          case AnimationType.FadeIn:
+            link.style.opacity= "1";
+            break;
+          case AnimationType.FadeOut:
+            link.style.opacity= "0";
+            break;
+        }
         link.style.transform = "translateY(0)";
       }, animationDelay + idx * elementAnimationDelay);
     });
