d3677d9ecec1fc8c0c00b550aaf7e9197eec1a2a
NikKowPHP <nik.kow@outlook.com>
2023-12-27 16:35:35 +0100

    fix(nav): Fix redirection issue and disable active page link
    Resolve the problem causing redirection to the same page and disable the link for the current active page in the navigation. This fix ensures smoother navigation and prevents redundant page reloading.

--- src/components/parts/Nav.tsx
+++ src/components/parts/Nav.tsx
@@ -1,57 +1,65 @@
 import React, { useEffect } from "react";
-import { useLinkAnimation,AnimationType } from "../hooks/useLinkAnimation";
-import { BASE_URL } from "../../constants/constants";
+import { useLinkAnimation, AnimationType } from "../hooks/useLinkAnimation";
 import { useRedirectContext } from "../contexts/RedirectContext";
+import { useLocation } from "react-router-dom";
+
+interface NavLink {
+  link: string;
+  path: string;
+}
 
 const Nav: React.FC = () => {
-  const {isRedirecting} = useRedirectContext();
-  const {handleLinkClick, animateLinksReverse} = useLinkAnimation([".nav-link" ],AnimationType.SlideIn,AnimationType.SlideOut, 250, 250);
+  const { isRedirecting } = useRedirectContext();
+  const { handleLinkClick, animateLinksReverse } = useLinkAnimation(
+    [".nav-link"],
+    AnimationType.SlideIn,
+    AnimationType.SlideOut,
+    250,
+    250
+  );
+  const location = useLocation();
+  const currentLocation = location.pathname;
+  const currentPage = currentLocation.split("/").slice(2, 3).toLocaleString();
+
   useEffect(() => {
     if (isRedirecting) animateLinksReverse();
   }, [isRedirecting]);
+
+  const navLinks: { [key: string]: NavLink } = {
+    home: { link: "welcome", path: "Home" },
+    about: { link: "about", path: "About" },
+    projects: { link: "projects", path: "Projects" },
+    contact: { link: "contact", path: "Projects" },
+  };
+
+  const handleClick = (link: string) => {
+    if (currentPage !== link) {
+      handleLinkClick(link);
+    }
+  };
 
   return (
     <nav className=" absolute top-0  w-full  bg-transparent  z-10 overflow-hidden">
       <div className="max-w-4x1 mx-auto">
         <ul className="flex justify-around list-none text-white font-semibold">
-          <li className="nav-link transition-transform transform -translate-y-full duration-1000 ">
-            <button
-              className="block p-4  font-semibold cursor-pointer hover:scale-110 text-opacity-75 text-whiteBlue"
-              onClick={() => handleLinkClick("")}
-            >
-              Home
-            </button>
-          </li>
-          <li className="nav-link transition-transform transform -translate-y-full duration-1000">
-            <button
-              className="block p-4  font-semibold cursor-pointer hover:scale-110  text-opacity-75 text-whiteBlue"
-              onClick={() => handleLinkClick("about")}
-            >
-              About
-            </button>
-          </li>
-          <li className="nav-link transition-transform transform -translate-y-full duration-1000 ">
-            <button
-              className="block p-4  font-semibold cursor-pointer hover:scale-110
-              text-opacity-75 text-whiteBlue
-              "
-              onClick={() => handleLinkClick("projects")}
-            >
-              Projects
-            </button>
-          </li>
-          <li
-            className="nav-link transition-transform transform -translate-y-full duration-1000 
-          text-opacity-75 text-whiteBlue
-          "
-          >
-            <button
-              className="block p-4  font-semibold cursor-pointer hover:scale-110  "
-              onClick={() => handleLinkClick("contact")}
-            >
-              Contact
-            </button>
-          </li>
+          {currentLocation &&
+            Object.keys(navLinks).map((key) => {
+              const { link, path } = navLinks[key];
+              return (
+                <li
+                  key={key}
+                  className="nav-link transition-transform transform -translate-y-full duration-1000 "
+                >
+                  <button
+                    className="block p-4  font-semibold cursor-pointer hover:scale-110 text-opacity-75 text-whiteBlue "
+                    disabled={currentPage === link}
+                    onClick={() => handleClick(link)}
+                  >
+                    {path}
+                  </button>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </nav>
