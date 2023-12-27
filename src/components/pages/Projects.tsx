9743d64510e08612e9a66e54634db3cd1927565f
NikKowPHP <nik.kow@outlook.com>
2023-12-27 11:12:19 +0100

    refactor(Projects): Modify styles and update project link to description
    Adjust styles within the Projects component and replace the click link with project descriptions. This modification improves the presentation and interaction by displaying project descriptions instead of clickable links.

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -5,7 +5,7 @@ import { useData } from "../hooks/useData";
 import Panels from "../parts/Panels";
 import { AnimationType, useLinkAnimation } from "../hooks/useLinkAnimation";
 import Footer from "../parts/Footer";
-
+import { useRedirectContext } from "../contexts/RedirectContext";
 
 const Projects: React.FC = () => {
   const [previosTextComplete, setPreviosTextComplete] =
@@ -15,20 +15,30 @@ const Projects: React.FC = () => {
 
   const texts = ["/projects", "Selected projects i've created"];
 
+  const { isRedirecting } = useRedirectContext();
 
   const { data, fetchData } = useData();
 
-    const {handleLinkClick} = useLinkAnimation([".card"], AnimationType.SlideIn, 600, 700,data );
+  const { handleLinkClick, animateLinksReverse } = useLinkAnimation(
+    [".card"],
+    AnimationType.SlideIn,
+    AnimationType.SlideOutReverse,
+    600,
+    700,
+    data
+  );
 
   const handleTypingComplete = (index: number) => {
     setCompletedBlocks((prevBlocks) => [...prevBlocks, index + 1]);
     setPreviosTextComplete(true);
   };
+  useEffect(() => {
+    if (isRedirecting) animateLinksReverse();
+  }, [isRedirecting]);
 
   useEffect(() => {
     fetchData();
   }, []);
-
 
   useEffect(() => {
     if (previosTextComplete) {
@@ -39,18 +49,17 @@ const Projects: React.FC = () => {
     }
   }, [previosTextComplete, currentTextIndex]);
 
-
   const renderHeader = () => {
     return (
       <header className="projects-header__title flex flex-col items-center justify-center ">
         {completedBlocks.includes(0) && (
-          <h1 className="text-center flex-1">
+          <h2 className="text-center text-5xl md:text-8xl lg:text-9xl mb-5 flex-1">
             <TextTypingEffect
               text={texts[0]}
               durationInMs={100}
               onComplete={() => handleTypingComplete(0)}
             />
-          </h1>
+          </h2>
         )}
         {completedBlocks.includes(1) && (
           <h3 className="text-center w-full flex-1">
@@ -78,7 +87,7 @@ const Projects: React.FC = () => {
             image={project.thumbnail}
             link={project.link}
             stack={project.stack}
-            onClick={() => handleLinkClick(project.link)}
+            onClick={() => handleLinkClick(`projects/${project.link}`)}
           />
         );
       })
@@ -87,7 +96,9 @@ const Projects: React.FC = () => {
 
   return (
     <>
-    <Panels texts={['cool projects', 'github streak', 'dedication', 'constance']} />
+      <Panels
+        texts={["cool projects", "github streak", "dedication", "constance"]}
+      />
       <div className="relative z-10 text-white mt-20 mb-4 h-auto pb-[56px]">
         {renderHeader()}
         <div className="mx-auto max-w-screen-xl mt-6 flex flex-wrap justify-center overflow-hidden">
