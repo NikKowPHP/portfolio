01bdd7037a087b5960bb611934a413f6b7fc6c92
NikKowPHP <nik.kow@outlook.com>
2023-12-27 11:10:26 +0100

    refactor(useLinkAnimation): Restructure animation handling based on animationType
    Reorganize the code within useLinkAnimation to encapsulate animation logic according to provided animationType and its reverse. This refactor enhances code organization for more streamlined animation handling.

--- src/components/hooks/useLinkAnimation.tsx
+++ src/components/hooks/useLinkAnimation.tsx
@@ -14,71 +14,68 @@ export enum AnimationType {
 export const useLinkAnimation = (
   selectors: string[],
   animationType: AnimationType = AnimationType.SlideIn,
+  animationTypeReverse: AnimationType = AnimationType.SlideOut,
   animationDelay: number = 3000,
   elementAnimationDelay: number = 400,
-  data: any | undefined = undefined,
-
+  data: any | undefined = undefined
 ) => {
   const navigate = useNavigate();
   const [links, setLinks] = useState<HTMLElement[]>([]);
   const { setIsRedirecting, isRedirecting } = useRedirectContext();
 
-
   const slideInAnimation = (link: HTMLElement) => {
-      link.style.transform = "translateY(0)";
+    link.style.transform = "translateY(0)";
   };
 
-  const slideOutAnimation = (link: HTMLElement ) => {
-      link.style.transform = "translateY(-150%)";
+  const slideOutAnimation = (link: HTMLElement) => {
+    link.style.transform = "translateY(-250%)";
   };
-  
+  const slideOutReverseAnimation = (link: HTMLElement) => {
+    link.style.transform = "translateY(+250%)";
+  };
 
   const fadeInAnimation = (link: HTMLElement) => {
-      link.style.opacity = "1";
+    link.style.opacity = "1";
   };
 
   const fadeOutAnimation = (link: HTMLElement) => {
-      link.style.opacity = "0";
+    link.style.opacity = "0";
   };
 
-  const reverseAnimationMap: Record<
-    AnimationType,
-    (link: HTMLElement) => void
-  > = {
-    [AnimationType.SlideIn]: slideOutAnimation,
-    [AnimationType.SlideOut]: slideInAnimation,
-    [AnimationType.SlideOutReverse]: slideInAnimation,
-    [AnimationType.FadeIn]: fadeOutAnimation,
-    [AnimationType.FadeOut]: fadeInAnimation,
+  const performAnimation = (selectedAnimation: AnimationType, link:HTMLElement) => {
+    switch (selectedAnimation) {
+      case AnimationType.SlideIn:
+        slideInAnimation(link);
+        break;
+      case AnimationType.SlideOut:
+        slideOutAnimation(link);
+        break;
+      case AnimationType.SlideOutReverse:
+        slideOutReverseAnimation(link);
+        break;
+      case AnimationType.FadeIn:
+        fadeInAnimation(link);
+        break;
+      case AnimationType.FadeOut:
+        fadeOutAnimation(link);
+        break;
+    }
   };
 
   const animateLinks = () => {
     links.forEach((link, idx) => {
       setTimeout(() => {
-        switch (animationType) {
-          case AnimationType.SlideIn:
-            slideInAnimation(link);
-            break;
-          case AnimationType.SlideOut:
-            slideOutAnimation(link);
-            break;
-          case AnimationType.FadeIn:
-            fadeInAnimation(link);
-            break;
-          case AnimationType.FadeOut:
-            fadeOutAnimation(link);
-            break;
-        }
+        performAnimation(animationType, link);
       }, animationDelay + idx * elementAnimationDelay);
     });
   };
+
   const animateLinksReverse = (): Promise<boolean> => {
     return new Promise((resolve) => {
       let completedAnimations = 0;
       links.forEach((link, idx) => {
         setTimeout(() => {
-          const reverseAnimation = reverseAnimationMap[animationType];
-          reverseAnimation(link)
+          performAnimation(animationTypeReverse, link);
           completedAnimations++;
           if (completedAnimations === links.length) {
             resolve(true);
@@ -87,6 +84,7 @@ export const useLinkAnimation = (
       });
     });
   };
+
   const handleLinkClick = async (
     navLink: string,
     isExternalLink: boolean = false
@@ -94,10 +92,24 @@ export const useLinkAnimation = (
     setIsRedirecting(true);
     await animateLinksReverse();
     setTimeout(() => {
-      !isExternalLink ? navigate(BASE_URL + navLink) : (window.location.href = navLink);
-    }, 500);
+      !isExternalLink
+        ? navigate(BASE_URL + navLink)
+        : (window.location.href = navLink);
+    }, 700);
     setIsRedirecting(false);
   };
+
+  const updateLinks = () => {
+    const allLinks: HTMLElement[] = [];
+    selectors.forEach((selector) => {
+      const links = document.querySelectorAll<HTMLElement>(selector);
+      const linksArray: HTMLElement[] = Array.from(links);
+      allLinks.push(...linksArray);
+    });
+
+    setLinks(allLinks);
+  };
+  
   useEffect(() => {
     animateLinks();
   }, [links, location.pathname]);
@@ -110,16 +122,5 @@ export const useLinkAnimation = (
     updateLinks();
   }, [data]);
 
-  const updateLinks = () => {
-    const allLinks: HTMLElement[] = [];
-    selectors.forEach((selector) => {
-      const links = document.querySelectorAll<HTMLElement>(selector);
-      const linksArray: HTMLElement[] = Array.from(links);
-      allLinks.push(...linksArray);
-    });
-
-    setLinks(allLinks);
-  };
-
-  return { links, animateLinksReverse, handleLinkClick  };
+  return { links, animateLinksReverse, handleLinkClick };
 };
